032b161d368db091ccf4d83429726a49ad7c6c78
error7410 <error7410@gmail.com>
2026-03-29 17:44:06 +0900

    관리자 사용자관리 삭제 UI추가

--- src/admin/pages/admin-user-page.tsx
+++ src/admin/pages/admin-user-page.tsx
@@ -3,63 +3,49 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { Calendar, LogIn, MoreVertical } from "lucide-react";
+import { Calendar, Mail, MoreVertical, Share2 } from "lucide-react";
 import { PopoverClose } from "@radix-ui/react-popover";
 import { Link } from "react-router";
 import Loader from "@/components/loader";
 import { useProfileAllData } from "@/hooks/queries/use-profile-all-data";
 import { formatTime } from "@/lib/time";
 
-const users = [
-  {
-    name: "김철수",
-    email: "chulsoo@example.com",
-    type: "이메일",
-    date: "2024.03.20",
-    lastLogin: "10분 전",
-    provider: "mail",
-  },
-  {
-    name: "이지현",
-    email: "jihyun_g@gmail.com",
-    type: "소셜",
-    date: "2024.03.18",
-    lastLogin: "2시간 전",
-    provider: "google",
-  },
-  {
-    name: "박준영",
-    email: "junyoung@kakao.com",
-    type: "소셜",
-    date: "2024.03.15",
-    lastLogin: "어제",
-    provider: "kakao",
-  },
-  {
-    name: "최수민",
-    email: "sumin_c@test.com",
-    type: "이메일",
-    date: "2024.03.10",
-    lastLogin: "3일 전",
-    provider: "mail",
-  },
-  {
-    name: "정은지",
-    email: "eunji@naver.com",
-    type: "이메일",
-    date: "2024.03.05",
-    lastLogin: "1주일 전",
-    provider: "mail",
-  },
-];
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function AdminUserPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const {
     data: profile,
     error: fetchProfileError,
     isLoading: isFetchingProfileLoading,
     fetchStatus: isFetchingProfileStatus,
   } = useProfileAllData();
+
+  const handleDeleteUserAction = () => {
+    toast("삭제 되었습니다.", {
+      position: "top-center",
+    });
+    // deleteQna(qnaId);
+  };
 
   if (isFetchingProfileLoading) {
     return <Loader />;
@@ -85,14 +71,15 @@ export default function AdminUserPage() {
           <div className="overflow-hidden rounded-xl border border-gray-200 bg-white">
             {/* 1. 데스크탑 뷰 (표 형식) */}
             <div className="hidden md:block">
-              <table className="w-full text-left text-sm">
+              <table className="w-full table-fixed text-left text-sm">
                 <thead className="border-b border-gray-200 bg-gray-50 font-semibold text-gray-500 uppercase">
                   <tr>
-                    <th className="px-6 py-4">사용자</th>
+                    <th className="px-6 py-4 text-center">이메일</th>
+                    <th className="px-6 py-4 text-center">자기소개</th>
                     <th className="px-6 py-4 text-center">가입경로</th>
-                    <th className="px-6 py-4">가입날짜</th>
-                    <th className="px-6 py-4">마지막 로그인</th>
-                    <th className="px-6 py-4 text-right">관리</th>
+                    <th className="px-6 py-4 text-center">가입날짜</th>
+
+                    <th className="px-6 py-4 text-center">관리</th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-100">
@@ -101,34 +88,44 @@ export default function AdminUserPage() {
                       <tr key={i} className="hover:bg-gray-50/50">
                         <td className="px-6 py-4">
                           <div>{user.email}</div>
-                          <div className="text-muted-foreground text-xs">
+                          <div className="text-xs text-blue-400">
                             {user.nickname}
                           </div>
                         </td>
-                        <td className="px-6 py-4 text-center">
-                          {/* <span
-                            className={`inline-flex items-center gap-1 rounded-full px-2 py-0.5 text-[11px] font-bold ${user.type === "이메일" ? "bg-gray-100 text-gray-600" : "bg-indigo-50 text-indigo-600"}`}
-                          > */}
-                          {/* {user.provider === "mail" ? (
+                        <td className="px-6 py-4">
+                          <TooltipProvider>
+                            <Tooltip delayDuration={300}>
+                              <TooltipTrigger asChild>
+                                <div className="block w-full truncate text-gray-700">
+                                  {user.bio || "소개글이 없습니다."}
+                                </div>
+                              </TooltipTrigger>
+                              <TooltipContent
+                                side="top"
+                                className="max-w-75 break-all"
+                              >
+                                <p>{user.bio || "소개글이 없습니다."}</p>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </td>
+                        <td className="px-6 py-4 text-center whitespace-nowrap">
+                          <span
+                            className={`inline-flex items-center gap-1 rounded-full px-2 py-0.5 text-[11px] font-bold ${user.provider_type === "email" ? "bg-gray-100 text-gray-600" : "bg-indigo-50 text-indigo-600"}`}
+                          >
+                            {user.provider_type === "email" ? (
                               <Mail size={10} />
                             ) : (
                               <Share2 size={10} />
-                            )} */}
-                          {/* {user.type} */}
-                          1111
-                          {/* </span> */}
+                            )}
+                            {user.provider_type}
+                          </span>
                         </td>
-                        <td className="px-6 py-4 text-gray-600">
+                        <td className="px-6 py-4 text-center text-gray-600">
                           {formatTime(user.created_at)}
                         </td>
-                        <td className="px-6 py-4">
-                          <div className="flex items-center gap-1.5 text-gray-700">
-                            {/* 접속 상태 표시등 */}
-                            {/* <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div> */}
-                            {formatTime(user.created_at)}
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 text-right">
+
+                        <td className="px-6 py-4 text-center">
                           <Popover>
                             <PopoverTrigger>
                               <MoreVertical
@@ -138,11 +135,12 @@ export default function AdminUserPage() {
                             </PopoverTrigger>
                             <PopoverContent className="mr-15 flex w-24 flex-col overflow-hidden p-0">
                               <PopoverClose asChild>
-                                <Link to={`/profile/`}>
-                                  <div className="hover:bg-muted cursor-pointer px-4 py-3 text-xs">
-                                    삭제
-                                  </div>
-                                </Link>
+                                <div
+                                  onClick={() => setIsOpen(true)}
+                                  className="hover:bg-muted cursor-pointer px-4 py-3 text-xs"
+                                >
+                                  삭제
+                                </div>
                               </PopoverClose>
                             </PopoverContent>
                           </Popover>
@@ -161,7 +159,7 @@ export default function AdminUserPage() {
                     <div className="flex items-center justify-between">
                       <div className="flex flex-col items-start gap-2">
                         <span>{user.email}</span>
-                        <div className="text-muted-foreground text-xs">
+                        <div className="text-xs text-blue-400">
                           {user.nickname}
                         </div>
                       </div>
@@ -184,26 +182,42 @@ export default function AdminUserPage() {
                       </Popover>
                     </div>
 
+                    <TooltipProvider>
+                      <Tooltip delayDuration={300}>
+                        <TooltipTrigger asChild>
+                          <div className="text-muted-foreground block truncate text-xs">
+                            {user.bio || "소개글이 없습니다."}
+                          </div>
+                        </TooltipTrigger>
+                        <TooltipContent
+                          side="top"
+                          className="max-w-75 break-all"
+                        >
+                          <p>{user.bio || "소개글이 없습니다."}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+
                     <div className="space-y-2">
                       {/* 가입경로 + 날짜 정보 라인 */}
                       <div className="flex flex-wrap items-center gap-x-4 gap-y-2 pt-1">
-                        {/* <span
-                          className={`rounded px-2 py-0.5 text-[10px] font-bold ${user.type === "이메일" ? "bg-gray-100 text-gray-600" : "bg-indigo-50 text-indigo-600"}`}
-                        > */}
-                        {/* {user.type} */}
-                        {/* </span> */}
+                        <span
+                          className={`rounded px-2 py-0.5 text-[10px] font-bold ${user.provider_type === "email" ? "bg-gray-100 text-gray-600" : "bg-indigo-50 text-indigo-600"}`}
+                        >
+                          {user.provider_type}
+                        </span>
                         <div className="flex items-center gap-1 text-[11px] text-gray-400">
                           <Calendar size={12} />
                           <span className="inline-block pt-0.75 align-middle">
                             {formatTime(user.created_at)}
                           </span>
                         </div>
-                        <div className="flex items-center gap-1 text-[11px] font-medium text-indigo-500">
+                        {/* <div className="flex items-center gap-1 text-[11px] font-medium text-indigo-500">
                           <LogIn size={12} />
                           <span className="inline-block pt-0.75 align-middle">
                             {formatTime(user.created_at)}
                           </span>
-                        </div>
+                        </div> */}
                       </div>
                     </div>
                   </div>
@@ -212,6 +226,29 @@ export default function AdminUserPage() {
           </div>
         </div>
       </div>
+
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>글삭제</AlertDialogTitle>
+            <AlertDialogDescription>
+              <span className="text-blue-400">"000"</span> 사용자를 정말 삭제
+              하시겠습니까?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="cursor-pointer">
+              취소
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="cursor-pointer"
+              onClick={handleDeleteUserAction}
+            >
+              삭제
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
